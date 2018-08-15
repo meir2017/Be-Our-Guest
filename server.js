ef58036d115d62c39bade2ebee1fdfa7e89adede
meir2017 <66meir46@gmail.com>
2018-08-16 01:33:57 +0300

    test meir

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
-const express = require('express')
-const app = express()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const inlineCss = require('nodemailer-juice');
+const app = express();
+
+const axios = require('axios');
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3001, () => console.log('Example app listening on port 3000!'))
+////// emil send
+
+app.get('/meir/:mytext', (req, res) => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'BeOurGuestMail@gmail.com',
+            pass: 'guest2018'
+        }
+    });
+    // transporter.use('compile', inlineCss());
+    var mailOptions = {
+        from: 'Be Our Guest ',
+        to: req.params.mytext,
+        subject: 'Sending Email using Node.js',
+        html: '<h1 style="color:lightskyblue">Welcome</h1><p>That was easy!</p>'
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.send('swnd mail to  ' + req.params.mytext)
+})
+////end
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, console.log('Server running on port', port));
