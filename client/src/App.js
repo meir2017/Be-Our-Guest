b02a63f734429644722f108df14e63048d0d37cc
meir2017 <66meir46@gmail.com>
2018-08-17 19:52:14 +0300

    eject

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,50 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import CreateEvent from './components/CreateEvent';
 import axios from 'axios';
+import { observer, inject } from 'mobx-react';
 
+
+// @inject("store");
+// @observer;
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userLog: false,
       Options: true,
+
       UserId: "",
+      username: "",
+      password: "",
+      email: "",
+      events: [],
+      guests: [],
+      categories: []
 
     }
   }
-  userLogin = (user, pass) => {
+  userLogin = (name, pass) => {
+    axios.post('/beOurGuest/login', { name: name, pass: pass })
+      .then(response => {
+        console.log((response.data))
+        if (response.data != "")
+          this.setState({ userLog: true, UserId: response.data })
 
-    this.setState({ userLog: true })
+        this.setState({
+          UserId: response.data._id,
+          username: name,
+          password: pass,
+          email: response.data.email,
+          events: response.data.events,
+          guests: response.data.guests,
+          categories: response.data.categories
+
+        })
+
+      }).catch(function (error) {
+        console.log(error);
+      });
+
   }
   userRegister = (user) => {
     //{inputText: "meir", emailText: "66meir46", passText: "1212", passConfirm: "1212"}
@@ -28,6 +58,8 @@ class App extends Component {
       axios.post('/beOurGuest/newUser', user)
         .then(response => {
           console.log((response.data))
+          this.setState({ UserId: response.data })
+
           this.setState({ userLog: true, UserId: response.data })
         })
     }
--- client/src/index.js
+++ client/src/index.js
@@ -6,5 +6,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css'; //reactstrap
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store/BeOurGuestStore';
+
+import { Provider } from 'mobx-react';
+console.log(store)
+window.store = store;
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
