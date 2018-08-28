a9fd520fdbe9491f555e05fe4637414330bd7673
meir2017 <66meir46@gmail.com>
2018-08-28 19:53:53 +0300

    store

--- Models/CategoryModel.js
+++ Models/CategoryModel.js
@@ -5,21 +5,10 @@ let Schema = mongoose.Schema;
 
 let categorySchema = new mongoose.Schema({
     name: String,
+    colorCode: String
 });
 
 let Category = mongoose.model('category', categorySchema);
 
 module.exports = Category;
 
-
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-
-let categorySchema = new mongoose.Schema({
-    nameCategory: String,
-    colorCode: String
-});
-
-let Guest = mongoose.model('guest', categorySchema);
-
-module.exports = Category;
