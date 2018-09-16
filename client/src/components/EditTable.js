3cac471506575f5742a966f7fbbb52d9431fa424
unknown <klirs123@gmail.com>
2018-09-16 17:07:02 +0300

    added edit table

--- client/src/components/EditTable.js
+++ client/src/components/EditTable.js
@@ -3,6 +3,7 @@ import toRenderProps from 'recompose/toRenderProps';
 import withState from 'recompose/withState';
 import { observer, inject } from 'mobx-react';
 import EditIcon from '@material-ui/icons/Edit';
+import axios from 'axios';
 import {
   Divider,
   Popover,
@@ -55,135 +56,147 @@ const styles = theme => ({
 @inject("store")
 @observer
 class EditTableModal extends Component {
-  handleClose = () => {
-
-    this.updateAnchorEl(null);
-    this.setState({
-      
-        // tableName: '',
-        // tableSize: "",
-        // category: '',
-        // Guests: [],
-        // open: false
-    });
-};
-
-// onClose={() => {
-//   updateAnchorEl(null);
-// }}
-  
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-        tableName: '',
-        tableSize: '',
-        categories: [],
-        Guests: [],
-        open: false,
+      anchorEl: null,
+      open: false,
+      tableName: '',
+      tableSize: '',
+      categories: [],
+      Guests: [],
     }
-}
-  render(){
+  }
+
+  handleToggle = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({
+      tableName: '',
+      tableSize: "",
+      category: '',
+      Guests: [],
+      open: false,
+      anchorEl: null
+    });
+  }
+  handleTextChange = event => {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+  onChangeCategory = e => {
+    this.setState({ category: e.target.value });
+    // let category = this.props.store.user.categories.find(category => category._id === e.target.value);
+    // this.setState({ tableName: category.name });
+  }
+  editTable = e => {
+    e.preventDefault();
+    let store = this.props.store;
+    let tableInfo = {
+      title: this.state.tableName,
+      maxGuests: this.state.tableSize,
+      category: this.state.category,
+    }
+    axios.post('/beOurGuest/updateTable/' + store.user.events[store.eventIndex]._id, tableInfo)
+      .then(response => {
+
+        console.log(" updete Table ->id  =" + response.data._id)
+        this.props.store.updateTable(response.data, '');
+
+      })
+      .catch(err => console.log('Error: ', err));
+    this.handleToggle();
+  }
+
+render() {
   const { classes } = this.props;
-
+  const { anchorEl } = this.state;
+  const open = Boolean(anchorEl);
+  let category = this.props.store.user.categories.find(category => category._id === this.props.table.category);
   return (
-    <WithState>
-      {({ anchorEl, updateAnchorEl }) => {
-        const open = Boolean(anchorEl);
-        return (
-          <React.Fragment>
-            <IconButton
-              aria-owns={open ? 'render-props-popover' : null}
-              aria-haspopup="true"
-              variant="contained"
-              onClick={event => {
-                updateAnchorEl(event.currentTarget);
-              }}
-              aria-label="Edit" className={classes.iconButton} >
-              <EditIcon className={classes.icon} />
-            </IconButton>
-            {/* <Button
-              aria-owns={open ? 'render-props-popover' : null}
-              aria-haspopup="true"
-              variant="contained"
-              onClick={event => {
-                updateAnchorEl(event.currentTarget);
-              }}
-            >
-              Open Popodgdsgdsver
-            </Button> */}
-            <Popover
-              id="render-props-popover"
-              open={open}
-              anchorEl={anchorEl}
-              onClose={() => {
-                updateAnchorEl(null);
-              }}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
-              }}
-            >
-              <Typography className={classes.typography}>The content of the Popover.</Typography>
-              <form className={classes.container} noValidate autoComplete="off" align="center">
-                <FormControl className={classes.formControl} align="left">
-                  <FormControl required>
-                    <InputLabel shrink htmlFor="category">Select category</InputLabel>
-                    <Select
-                      value={this.state.category}
-                      displayEmpty
-                      onChange={this.onChangeCategory}
-                      inputProps={{
-                        name: 'category',
-                        id: 'category',
-                      }}
-                      displayEmpty
-                      autoWidth
-                    >
+    <React.Fragment>
+      <IconButton
+        aria-owns={open ? 'render-props-popover' : null}
+        aria-haspopup="true"
+        variant="contained"
+        onClick={this.handleToggle}
+        // onClick={event => {
+        //   updateAnchorEl(event.currentTarget);
+        // }}
+        aria-label="Edit" className={classes.iconButton} >
+        <EditIcon className={classes.icon} />
+      </IconButton>
+      <Popover
+        id="render-props-popover"
+        open={open}
+        anchorEl={anchorEl}
+        onClose={this.handleClose}
+        // onClose={() => {
+        //   updateAnchorEl(null);
+        // }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <form className={classes.container} noValidate autoComplete="off" align="center">
+          <FormControl className={classes.formControl} align="left">
+            <FormControl required>
+              <InputLabel shrink htmlFor="category">Select category</InputLabel>
+              <Select
+                defaultValue={category.name}
+                displayEmpty
+                onChange={this.onChangeCategory}
+                inputProps={{
+                  name: 'category',
+                  id: 'category',
+                }}
+                displayEmpty
+                autoWidth >
 
-                      {this.props.store.user.categories.map((item, index) => {
-                        return <MenuItem value={item._id} key={item._id}>{item.name}</MenuItem>
-                      })}
+                {this.props.store.user.categories.map((item, index) => {
+                  return <MenuItem value={item._id} key={item._id}>{item.name}</MenuItem>
+                })}
 
-                    </Select>
-                  </FormControl>
-                  <TextField
-                    id="tableName"
-                    label="Table name"
-                    type="text"
-                    className={classes.textField}
-                    value={this.state.tableName}
-                    placeholder="Enter table name"
-                    onChange={this.handleTextChange}
-                    margin="normal" />
-                  <TextField
-                    id="tableSize"
-                    label="Max number of guests"
-                    type="number"
-                    required
-                    value={this.state.tableSize}
-                    placeholder="Enter number of places"
-                    onChange={this.handleTextChange}
-                    className={classes.textField}
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                    margin="normal" />
-                     <Divider></Divider>
-                        <Button size="small" onClick={this.handleClose}>Cancel</Button>
-                        <Button size="small" color="primary" onClick={this.addTable} > Save </Button>
-                </FormControl>
-              </form>
-            </Popover>
-          </React.Fragment>
-        );
-      }}
-    </WithState>
+              </Select>
+            </FormControl>
+            <TextField
+              id="tableName"
+              label="Table name"
+              type="text"
+              className={classes.textField}
+              placeholder="Enter table name"
+              defaultValue={this.props.table.title}
+              onChange={this.handleTextChange}
+              margin="normal" />
+            <TextField
+              id="tableSize"
+              label="Max number of guests"
+              type="number"
+              defaultValue={this.props.table.maxGuests}
+              required
+              placeholder="Enter number of places"
+              onChange={this.handleTextChange}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal" />
+            <Divider></Divider>
+            <Button size="small" onClick={this.handleClose}>Cancel</Button>
+            <Button size="small" color="primary" onClick={this.editTable} > Save </Button>
+          </FormControl>
+        </form>
+      </Popover>
+    </React.Fragment>
   );
 }
+
 }
 
 export default withStyles(styles)(EditTableModal);
