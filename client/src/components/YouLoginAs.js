90f910fae650545145877e310641d55b658b88ec
meir2017 <66meir46@gmail.com>
2018-09-16 03:44:55 +0300

    go01

--- client/src/components/YouLoginAs.js
+++ client/src/components/YouLoginAs.js
@@ -9,8 +9,8 @@ class YouLoginAs extends Component {
     constructor(props) {
         super(props);
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="youLoggedAs">
                 <h3> {this.props.store.user.username} </h3>
             </div>
