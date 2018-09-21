4badc55e5eba7b3d9472146f7845bebbf97eeed0
meir2017 <66meir46@gmail.com>
2018-09-21 17:34:20 +0300

    navber

---
+++ client/src/components/DropdownEvent.js
@@ -0,0 +1,83 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { observer, inject } from 'mobx-react';
+
+const styles = theme => ({
+    root: {
+        display: "flex",
+        flexWrap: "wrap"
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120
+    },
+    selectEmpty: {
+        marginTop: theme.spacing.unit * 2,
+        color: "green"
+    }
+
+});
+
+@inject("store")
+@observer
+class SimpleSelect extends React.Component {
+    state = {
+        event: "Select Event",
+        name: "hai"
+    };
+
+    handleChange = event => {
+        // this.setState({ [event.target.name]: event.target.value });
+        if (event.target.value !== null) {
+            this.props.store.thisEventIndex(event.target.value)
+            this.props.store.ChangeMyEventPage(true)
+        }
+    };
+
+    componentDidMount() {
+        this.setState({ event: this.props.store.eventIndex });
+    }
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <form className={classes.root} className="SelectEvent"
+                autoComplete="off" style={{ color: "white" }}>
+                <FormControl className={classes.formControl}>
+                    <Select
+                        className="SelectEvent"
+                        style={{ color: "white" }}
+                        value={this.state.event}
+                        onChange={this.handleChange}
+                        value={this.props.store.eventIndex}
+                        displayEmpty
+                        name="event"
+                        className={classes.selectEmpty}
+                    >
+                        <MenuItem value={null}>
+                            <em>Select Event</em>
+                        </MenuItem>
+                        {this.props.store.user.events.length === 0 ? <MenuItem> No events </MenuItem> : this.props.store.user.events.map((eve, index) => {
+                            return <MenuItem value={index} key={eve.HostName + eve.Location + index} id={index}
+                            >    {eve.Title}</MenuItem>
+                        })}
+
+
+                    </Select>
+                    <FormHelperText>Without label</FormHelperText>
+                </FormControl>
+            </form>
+        );
+    }
+}
+
+SimpleSelect.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SimpleSelect);
--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,92 +1,148 @@
-import React, { Component } from 'react';
-// import { FormErrors } from './FormErrors';
-// import './Form.css';
+// import React from "react";
+// import PropTypes from "prop-types";
+// import { withStyles } from "@material-ui/core/styles";
+// import Input from "@material-ui/core/Input";
+// import InputLabel from "@material-ui/core/InputLabel";
+// import MenuItem from "@material-ui/core/MenuItem";
+// import FormHelperText from "@material-ui/core/FormHelperText";
+// import FormControl from "@material-ui/core/FormControl";
+// import Select from "@material-ui/core/Select";
 
-class Form extends Component {
+// const styles = theme => ({
+//     root: {
+//         display: "flex",
+//         flexWrap: "wrap"
+//     },
+//     formControl: {
+//         margin: theme.spacing.unit,
+//         minWidth: 120
+//     },
+//     selectEmpty: {
+//         marginTop: theme.spacing.unit * 2
+//     }
+// });
+
+// class SimpleSelect extends React.Component {
+//     state = {
+//         event: "",
+//         name: "hai"
+//     };
+
+//     handleChange = event => {
+//         this.setState({ [event.target.name]: event.target.value });
+//     };
+
+//     render() {
+//         const { classes } = this.props;
+
+//         return (
+//             <form className={classes.root} autoComplete="off">
+//                 <FormControl className={classes.formControl}>
+//                     <Select
+//                         value={this.state.event}
+//                         onChange={this.handleChange}
+//                         inputProps={{
+//                             name: "event",
+//                             id: "age-simple"
+//                         }}
+//                     >
+//                         <MenuItem value="">
+//                             <em>My Event</em>
+//                         </MenuItem>
+//                         <MenuItem value={10}>Ten</MenuItem>
+//                         <MenuItem value={20}>Twenty</MenuItem>
+//                         <MenuItem value={30}>Thirty</MenuItem>
+//                     </Select>
+//                 </FormControl>
+//             </form>
+//         );
+//     }
+// }
+
+// SimpleSelect.propTypes = {
+//     classes: PropTypes.object.isRequired
+// };
+
+// export default withStyles(styles)(SimpleSelect);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import { Button, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { observer, inject } from 'mobx-react';
+
+
+@inject("store")
+@observer
+export default class Example extends React.Component {
     constructor(props) {
         super(props);
+
+        this.toggle = this.toggle.bind(this);
         this.state = {
-            email: '',
-            password: '',
-            formErrors: { email: '', password: '' },
-            emailValid: false,
-            passwordValid: false,
-            formValid: false
-        }
+            dropdownOpen: false
+        };
     }
 
-    handleUserInput = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({ [name]: value },
-            () => { this.validateField(name, value) });
-    }
-
-    validateField(fieldName, value) {
-        let fieldValidationErrors = this.state.formErrors;
-        let emailValid = this.state.emailValid;
-        let passwordValid = this.state.passwordValid;
-
-        switch (fieldName) {
-            case 'email':
-                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-                fieldValidationErrors.email = emailValid ? 'good' : ' is invalid';
-                break;
-            case 'password':
-                passwordValid = value.length >= 6;
-                fieldValidationErrors.password = passwordValid ? 'good' : ' is too short';
-                break;
-            default:
-                break;
-        }
+    toggle() {
         this.setState({
-            formErrors: fieldValidationErrors,
-            emailValid: emailValid,
-            passwordValid: passwordValid
-        }, this.validateForm);
+            dropdownOpen: !this.state.dropdownOpen
+        });
     }
 
-    validateForm() {
-        this.setState({ formValid: this.state.emailValid && this.state.passwordValid });
+    myEvent = () => {
+        this.props.store.user.events.map((eve, index) => {
+            return <DropdownItem name={index} key={eve.HostName + eve.Location + index} id={index}
+                onClick={(e) => { this.handleEvent(index) }} >    {eve.Title}</DropdownItem>
+        })
     }
 
-    errorClass(error, item) {
-        if (item.length !== 0)
-            return (error === "good" ? 'noError' : 'has-error');
-        else return "emty"
-    }
+    handleEvent = (index) => {
+        this.props.store.thisEventIndex(index)
+        this.props.store.ChangeMyEventPage(true)
 
+    }
 
     render() {
-        const item = this.state
         return (
-            <form className="demoForm">
-                <h2>Sign up</h2>
-                <div className="panel panel-default">
-                    {/* <FormErrors formErrors={this.state.formErrors} /> */}
-                </div>
-                <div className={`form-group ${this.errorClass(item.formErrors.email, item.email)}`}>
-                    <label htmlFor="email">Email address</label>
-                    <input type="email" required className="form-control" name="email"
-                        placeholder="Email"
-                        value={this.state.email}
-                        onChange={this.handleUserInput} />
-                </div>
-                {/* {this.state.formErrors.email === ' is invalid' ? ' is invalid' : ''} */}
-                <div className={`form-group ${this.errorClass(item.formErrors.password, item.password)}`}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" name="password"
-                        placeholder="Password"
-                        value={this.state.password}
-                        onChange={this.handleUserInput} />
-                </div>
-                {/* {this.state.formErrors.password === ' is too short' ? ' is too short' : ''} */}
-                <br />
+            <ButtonDropdown isOpen={this.state.dropdownOpen} className="dropdownOpen" toggle={this.toggle}>
+                <DropdownToggle caret className="btn-dropdown" >
+                    My Events
+                </DropdownToggle>
 
-                <button type="submit" className="btn btn-primary" disabled={!item.formValid}>Sign up</button>
-            </form>
-        )
+                <DropdownMenu>
+                    {this.props.store.user.events.length === 0 ? <DropdownItem> No events </DropdownItem> : this.props.store.user.events.map((eve, index) => {
+                        return <DropdownItem name={index} key={eve.HostName + eve.Location + index} id={index}
+                            onClick={(e) => { this.handleEvent(index) }} >    {eve.Title}</DropdownItem>
+                    })}
+                </DropdownMenu>
+            </ButtonDropdown >
+        );
     }
 }
-
-export default Form
