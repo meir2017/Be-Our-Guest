09e446c6935a71173f5793a8954119337a918906
meir2017 <66meir46@gmail.com>
2018-10-04 15:34:56 +0300

    meir

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ io.on('connection', socket => {
     })
 })
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build', 'index.html')));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 //***************  Email ***************//
