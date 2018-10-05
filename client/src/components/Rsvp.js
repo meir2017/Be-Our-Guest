b05682d5b731df49bf452d11a7837057747201a5
meir2017 <66meir46@gmail.com>
2018-10-05 03:46:10 +0300

    make it better

--- client/src/components/Rsvp.js
+++ client/src/components/Rsvp.js
@@ -1,9 +1,15 @@
-import React, { Component } from "react";
-import { Button, Form } from "reactstrap";
-import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
-import socketIOClient from "socket.io-client";
-import axios from "axios";
-import { observer, inject } from "mobx-react";
+import React, { Component } from 'react';
+import Checkbox from "@material-ui/core/Checkbox";
+import { Button, Form } from 'reactstrap';
+
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+// import logo from '../../.public/loading.jpg';
+// import logo from '../../public/loading.jpg';
+
+import axios from 'axios';
+import { observer, inject } from 'mobx-react';
 @inject("store")
 @observer
 class Rsvp extends Component {
@@ -11,8 +17,8 @@ class Rsvp extends Component {
     super(props);
     this.state = {
       invitationName: "",
-      titleInput: "",
-      textInput: "",
+      titleInput: '',
+      textInput: '',
       background: "",
       titleColor: "",
       bodyColor: "",
@@ -21,249 +27,128 @@ class Rsvp extends Component {
       whenEvent: "",
       whereEvent: "",
 
-      arryComing: [],
-      arryNotComing: [],
-      coming: 0,
-      notComing: 0,
-      numInvited: "",
-      vient: "",
-      returnRsvp: false,
-      // endpoint: "http://127.0.0.1:3001"
-      endpoint: "https://beourguest.herokuapp.com",
-      // endpoint: "https://beourguest.herokuapp.com/socket.io/?EIO=4&transport=websocket",
-    };
+
+      checkedB: false,
+      numGuest: 0,
+      myText: "",
+
+      display_rsvp: false
+    }
   }
-  send = () => {
-    debugger;
-    let guestId = this.props.match.params.guestId;
-    let eventId = this.props.match.params.eventId;
-    const socket = socketIOClient(this.state.endpoint);
-    socket.emit("callRsvp", {
-      coming: this.state.coming,
-      notComing: this.state.notComing,
-      guestId: guestId,
-      eventId: eventId
-    });
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+    // alert(this.state[name])
   };
-
-  inSocket = () => {
-    // console.log(" befer connected ")
-    const socket2 = socketIOClient(this.state.endpoint);
-    socket2.on("connect", () => {
-      console.log("connected ");
-    });
-    // console.log(socket2)
+  handleChangeGuest = name => event => {
+    this.setState({ [name]: event.target.value });
+    // alert(this.state[name])
   };
-
-  onSelectConfirmed = e => {
-    let coming = this.state.coming;
-    let notComing = this.state.notComing;
-    let numInvited = this.state.numInvited;
-    let vient = this.state.vient;
-    if (e.target.name === "coming") {
-      coming = e.target.value;
-      numInvited = vient - coming - notComing;
-      notComing = vient - coming;
-    }
-
-    if (e.target.name === "notComing") {
-      notComing = e.target.value;
-      numInvited = vient - coming - notComing;
-      coming = vient - notComing;
-    }
-
-    let setArryComing = [];
-    let setArryNotComing = [];
-
-    for (let index = 0; index <= coming; index++) {
-      setArryComing.push(index);
-    }
-    for (let index = 0; index <= notComing; index++) {
-      setArryNotComing.push(index);
-    }
-
-    this.setState({
-      [e.target.name]: e.target.value,
-      arryComing: setArryComing,
-      arryNotComing: setArryNotComing,
-      numInvited: numInvited
-    });
-  };
-  Submitfunc = e => {
+  Submitfunc = (e) => {
     e.preventDefault();
-    this.send();
-
+    console.log(this.state.numGuest);
+    console.log(this.state.checkedB);
     //send the info to evntid,gustid
     let objRsvp = {
-      coming: this.state.coming,
-      notComing: this.state.notComing,
-      numInvited: this.state.numInvited,
-      guestId: this.props.match.params.guestId,
-      eventId: this.props.match.params.eventId
-    };
-    axios.post("/beOurGuest/rsvp/guestAnswer/", objRsvp).then(response => {
-      // console.log((response.data))
-    });
-    this.toggleSendRsvp(e);
-  };
+      checkedB: this.state.checkedB,
+      numGuest: this.state.numGuest,
+      guestId: this.props.match.params.guestId
+    }
+    console.log(this.props.match.params.guestId)
+    axios.post('/beOurGuest/rsvp/guestAnswer/', objRsvp)
+      .then(response => {
+        console.log((response.data))
+        // this.props.store.addInvitation(response.data)
+      })
+  }
 
   componentWillMount = () => {
-    this.inSocket();
-    let guestId = this.props.match.params.guestId;
-
-    axios.get(`/beOurGuest/rsvpGuest/guestId/${guestId}`).then(response => {
-      // console.log("rsvpGuest")
-      let item = response.data;
-      this.setState({
-        numInvited: item.numInvited,
-        vient: item.numInvited
-      });
-
-      let newArry = [];
-      for (let index = 0; index <= item.numInvited; index++) {
-        newArry.push(index);
-      }
-      this.setState({ arryComing: newArry, arryNotComing: newArry });
-      this.props.ChangeToRsvpPage();
-      this.getUserInfo();
-    });
-  };
-  toggleSendRsvp = e => {
-    e.preventDefault();
-
-    this.setState({
-      returnRsvp: !this.state.returnRsvp
-    });
-  };
-
+    this.props.ChangeToRsvpPage();
+    this.getUserInfo();
+  }
   getUserInfo = () => {
+
+    // let infoEvent = {
+    //     vetId: this.props.match.params.vetId,
+    //     eventId: this.props.match.params.eventId,
+    //     guestId: this.props.match.params.guestId
+    // }
+
     let vetId = this.props.match.params.vetId;
-    axios.get(`/beOurGuest/rsvpGuest/${vetId}`).then(response => {
-      console.log("rsvpGuest getUserInfo");
-      let item = response.data;
-      this.setState({
-        invitationName: item.invitationName,
-        titleInput: item.titleInput,
-        textInput: item.textInput,
-        whenEvent: item.whenEvent,
-        whereEvent: item.whereEvent,
-        background: item.background,
-        titleColor: item.titleColor,
-        bodyColor: item.bodyColor,
-        fontTitle: item.fontTitle,
-        fontBody: item.fontBody,
-        display_rsvp: true
-      });
-    });
-  };
+    axios.get(`/beOurGuest/rsvpGuest/${vetId}`)
+      .then(response => {
+        console.log("rsvpGuest")
+        let item = response.data;
+        this.setState({
+          invitationName: item.invitationName,
+          titleInput: item.titleInput,
+          textInput: item.textInput,
+          whenEvent: item.whenEvent,
+          whereEvent: item.whereEvent,
+          background: item.background,
+          titleColor: item.titleColor,
+          bodyColor: item.bodyColor,
+          fontTitle: item.fontTitle,
+          fontBody: item.fontBody,
+          display_rsvp: true
+
+        })
+      })
+
+  }
 
   render() {
     return (
       <div>
         <div className="row">
-          <div className="col-sm-3" />
-          <div className="col-sm-6">
-            <br />
-            <Form
-              className="display_rsvp"
-              onSubmit={this.toggleSendRsvp}
-              style={{ backgroundColor: `${this.state.background}` }}
-            >
-              <div>
-                <h2
-                  style={{
-                    color: `${this.state.titleColor}`,
-                    fontFamily: `${this.state.fontTitle}`
-                  }}
-                >
-                  {this.state.titleInput}
-                </h2>
-                <div
-                  style={{
-                    whiteSpace: "pre-wrap",
-                    padding: "10px",
-                    color: `${this.state.bodyColor}`,
-                    fontFamily: `${this.state.fontBody}`
-                  }}
-                >
-                  <h4>{this.state.textInput}</h4>
+          <div className="col-sm-3"></div>
+          <div className="col-sm-6" >
+            <br /><br />
+            <Form className="display_rsvp" onSubmit={this.Submitfunc} style={{ backgroundColor: `${this.state.background}` }} >
+              <div  >
+                <h2 style={{ color: `${this.state.titleColor}`, fontFamily: `${this.state.fontTitle}` }}>{this.state.titleInput}</h2>
+                <div style={{ whiteSpace: "pre-wrap", padding: "10px", color: `${this.state.bodyColor}`, fontFamily: `${this.state.fontBody}` }}>
+                  <h4 >{this.state.textInput}</h4>
                 </div>
-                <p>
-                  {this.state.whenEvent} <br /> {this.state.whereEvent}
-                </p>
+                <p>{this.state.whenEvent}  <br />  {this.state.whereEvent}</p>
               </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-4">
-                  <label htmlFor="">Coming</label>
-                  <div className="rsvpInput">
-                    {/* {this.SetComing(this.state.coming)} */}
-                    <select
-                      className="form-control"
-                      id="coming"
-                      value={this.state.coming}
-                      onChange={this.onSelectConfirmed}
-                      name="coming"
-                    >
-                      {this.state.arryComing.map((item, index) => {
-                        return <option key={index + item}>{item} </option>;
-                      })}
-                    </select>
-                  </div>
-                </div>
-                <div className="col-sm-4 rsvpForm m2">
-                  <label htmlFor="">Not Coming</label>
-                  <div className="rsvpInput">
-                    <select
-                      className="form-control"
-                      id="notComing"
-                      value={this.state.notComing}
-                      onChange={this.onSelectConfirmed}
-                      name="notComing"
-                    >
-                      {this.state.arryNotComing.map((item, index) => {
-                        return <option key={index + item}>{item} </option>;
-                      })}
-                    </select>
-                  </div>
-                </div>
-                <div className="col-sm-4">
-                  <label htmlFor="">Undecided</label>
+              are you caming:   <Checkbox
+                checked={this.state.checkedB}
+                onChange={this.handleChange("checkedB")}
+                value="checkedB"
+                color="primary"
+              />
+              <br /><br />
 
-                  <div className="Undecided" id="Undecided">
-                    <div className="Undecided1">{this.state.numInvited}</div>
-                  </div>
-                </div>
-              </div>
+              <FormControl>
+                How many will you come?  <InputLabel htmlFor="age-native-simple"></InputLabel>
+                <Select
+                  native
+                  value={this.state.numGuest}
+                  onChange={this.handleChangeGuest('numGuest')}  >
+                  <option value="" />
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={4}>4</option>
+                  <option value={5}>5</option>
+                  <option value={6}>6</option>
+                  {/* {this.props.store.user.categories.map((item, index) => {
+                                     return   <option key={item._id} value={1}>{item.name}</option>
+                                    })} */}
+                </Select>
+              </FormControl>
+              <br />
+              <br />
               <br />
               <Button>Submit</Button>
-              <br /> <br />
+              <br />
+              <br />
             </Form>
           </div>
-          <div className="col-sm-3" />
+          <div className="col-sm-3"></div>
         </div>
 
-        <Modal
-          className="modalm"
-          style={{ width: "240px" }}
-          isOpen={this.state.returnRsvp}
-          toggle={this.toggleSendRsvp}
-        >
-          <ModalHeader toggle={this.toggle}>
-            Do you want to send your rsvp?
-          </ModalHeader>
-          <ModalFooter className="btnSend">
-            <Button
-              onClick={this.Submitfunc}
-              style={{ backgroundColor: "#560027" }}
-            >
-              Yes
-            </Button>
-            <Button onClick={this.toggleSendRsvp} color="secondary">
-              No
-            </Button>
-          </ModalFooter>
-        </Modal>
+
       </div>
     );
   }
